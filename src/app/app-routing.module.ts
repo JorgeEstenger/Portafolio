0c8050ffcfbd4a7136435f81a1f3527728cc443b
Jorge Estenger <jorgeestenger9@gmail.com>
2023-06-14 19:56:47 -0400

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactmeComponent } from './components/contactme/contactme.component';
+import { BriefcaseComponent } from './components/briefcase/briefcase.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'start', component: StartComponent},
+  {path:'skills', component: SkillsComponent},
+  {path: 'contactme', component: ContactmeComponent},
+  {path: 'briefcase', component: BriefcaseComponent},
+  {path: '**', pathMatch: 'full', redirectTo:'start' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
